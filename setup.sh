9fc11405d9302559813356d9b5f41f83ff38ed57
Austin Wulf <atwulf@gmail.com>
2018-05-13 08:50:46 -0600

    add nvm, rvm, and brew/postgres to setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
+# env setup
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 mkdir ~/Repos
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# install rvm
+curl -sSL https://get.rvm.io | bash -s stable
+
+# install brew stuff
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install postgresql
+
+source ~/.bash_profile
