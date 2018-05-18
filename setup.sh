a7ad331e58621c1af57f7dc9f844da5d6c28e95a
Austin Wulf <atwulf@gmail.com>
2018-05-17 20:34:50 -0600

    log when things are happening, add monokai terminal theme

--- setup.sh
+++ setup.sh
@@ -1,18 +1,34 @@
 # env setup
+printf "Setting up developer environment...\n\n"
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 mkdir ~/Repos
 
 # install nvm
+printf "Installing NVM...\n\n"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # install rvm
+printf "Installing RVM...\n\n"
 curl -sSL https://get.rvm.io | bash -s stable
 
 # install brew stuff
+printf "Installing Homebrew...\n\n"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew install postgresql
 
-source ~/.bash_profile
+# install postgres
+printf "Installing Postgres...\n\n"
+brew install postgresql
+brew services start postgresql
+
+# install monokai terminal theme
+printf "Installing Monokai Terminal theme...\n\n"
+mkdir tmp
+curl -o tmp/monokai.terminal 'https://raw.githubusercontent.com/stephenway/monokai.terminal/master/Monokai.terminal'
+open tmp/monokai.terminal
+rm -r tmp
+
+printf "All done! Don't forget to set Monokai as the default theme.\n\n"
+
